e05cff12fa5fc5f15a1363b69bf974f67011ec71
Mihailpp99 <mihailpp99@gmail.com>
2021-11-22 19:42:10 +0200

    Added 404 not found id for groups

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,5 +1,6 @@
 const Group = require("../models/groupModel")
 const catchAsync = require("../utils/catchAsync")
+const AppError = require("./../utils/appError")
 exports.checkId = (req,res,next,val)=>{
 
     // if(val){
@@ -34,7 +35,20 @@ exports.getAllGroups = catchAsync(async (req,res,next)=>{
 })
 
 exports.getGroup = catchAsync (async (req,res,next)=>{
-    const group = await Group.findById(req.params.id)
+    try{
+        const group = await Group.findById(req.params.id);
+    }catch(err){
+        return next(new AppError(`No group found with that ID`, 404))
+    }
+
+    const group = await Group.findById(req.params.id);
+    
+
+    // this if doesn't work because the await const is catched from catchAsync and the code execution for this function stops at const group
+    // if(!group){
+    //     return next(new AppError(`No group found with that ID`, 404))
+    // }
+
     res.status(200).json({
         status: "success",
         data: {group}
@@ -42,6 +56,15 @@ exports.getGroup = catchAsync (async (req,res,next)=>{
 })
 
 exports.updateGroup = catchAsync (async (req,res,next)=>{
+    try{
+        const group = await Group.findByIdAndUpdate(req.params.id, req.body,{
+            new: true,   // returns the new updated group
+            runValidators: true 
+        })
+    }catch(err){
+        return next(new AppError(`No group found with that ID`, 404))
+    }
+
     const group = await Group.findByIdAndUpdate(req.params.id, req.body,{
         new: true,   // returns the new updated group
         runValidators: true 
@@ -54,6 +77,11 @@ exports.updateGroup = catchAsync (async (req,res,next)=>{
 });
 
 exports.deleteGroup =catchAsync (async (req,res,next)=>{
+    try{
+        const group = await Group.findByIdAndDelete(req.params.id)
+    }catch(err){
+        return next(new AppError(`No group found with that ID`, 404))
+    }
     const group = await Group.findByIdAndDelete(req.params.id)
 
     res.status(204).json({
